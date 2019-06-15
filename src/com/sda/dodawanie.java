ca690a2ce34063ee48dd74094c8fe091c0571454
Patryk Ziemiński <patryk.zieminski@gmail.com>
2019-06-15 15:45:56 +0200

    Nie wyszło narazie xD

---
+++ src/com/sda/dodawanie.java
@@ -0,0 +1,14 @@
+package com.sda;
+
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+public class dodawanie {
+    public int dodawanie;
+    int a;
+    int b;
+    int dzialanie = a + b;
+
+
+}
